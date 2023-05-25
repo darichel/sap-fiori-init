8d08936af5bfc943f695987b25308cd3c09e7d15
Darichel Perez <darichel.perez@gmail.com>
2023-05-25 19:39:57 +0000

    Apply filters on list data

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -1,12 +1,16 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      * @param {typeof sap.ui.model.json.JSONModel} JSONModel
+     * @param {typeof sap.ui.model.Filter} Filter
+     * @param {typeof sap.ui.model.FilterOperator} FilterOperator
      */
-    function (Controller, JSONModel) {
+    function (Controller, JSONModel, Filter, FilterOperator) {
         "use strict";
 
         return Controller.extend("invoices.controller.MainView", {
@@ -16,13 +20,30 @@ sap.ui.define([
                 oJSONmodel.loadData("../model/SelectionScreenMenu.json");
                 oView.setModel(oJSONmodel, "SelectionScreen");
             },
+            
+            applyFilter: function (afilter) {
+                const oList = this.getView().byId("_IDGenList1");
+                oList.getBinding("items").filter(afilter);
+            },           
             onFilter: function (oEvent) {
-                
+                const oData = this.getView().getModel("SelectionScreen").getData()
+                let filters = [];
+
+                if (oData.ShipName !== "") {
+                    filters.push(new Filter("ShipName", FilterOperator.Contains, oData.ShipName));
+                }
+                if (oData.CountryKey!== "") {
+                    filters.push(new Filter("Country", FilterOperator.EQ, oData.CountryKey));
+                }
+
+                this.applyFilter(filters);
             },
             onClearFilter: function (oEvent) {
                 const oModelSelScreen = this.getView().getModel("SelectionScreen");
                 oModelSelScreen.setProperty("/ShipName", "");
                 oModelSelScreen.setProperty("/CountryKey", "");
+
+                this.applyFilter([]);
             },
         });
     });
