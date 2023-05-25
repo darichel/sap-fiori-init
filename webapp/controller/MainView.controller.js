d3ac7ac7739f793399de111b4e26729d80181df0
Darichel Perez <darichel.perez@gmail.com>
2023-05-25 16:47:53 +0000

    Add json file model and the list witch data

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -1,15 +1,20 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
+     * @param {typeof sap.ui.model.json.JSONModel} JSONModel
      */
-    function (Controller) {
+    function (Controller, JSONModel) {
         "use strict";
 
         return Controller.extend("invoices.controller.MainView", {
             onInit: function () {
-
+                const oJSONmodel = new JSONModel();
+                const oView = this.getView();
+                oJSONmodel.loadData("../model/SelectionScreenMenu.json");
+                oView.setModel(oJSONmodel, "SelectionScreenMenu");
             }
         });
     });
