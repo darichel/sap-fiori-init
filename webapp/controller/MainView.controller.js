03043ef4220b05e4ad2223b2967593c78349cf56
Darichel Perez <darichel.perez@gmail.com>
2023-05-25 17:55:57 +0000

    Add in destination a new odata service Northwind

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -14,7 +14,15 @@ sap.ui.define([
                 const oJSONmodel = new JSONModel();
                 const oView = this.getView();
                 oJSONmodel.loadData("../model/SelectionScreenMenu.json");
-                oView.setModel(oJSONmodel, "SelectionScreenMenu");
-            }
+                oView.setModel(oJSONmodel, "SelectionScreen");
+            },
+            onFilter: function (oEvent) {
+                
+            },
+            onClearFilter: function (oEvent) {
+                const oModelSelScreen = this.getView().getModel("SelectionScreen");
+                oModelSelScreen.setProperty("/ShipName", "");
+                oModelSelScreen.setProperty("/CountryKey", "");
+            },
         });
     });
